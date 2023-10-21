fe3d715147924a7ca4d07e5843bdd8e0710675de
Han Wang <hanforwork896@gmail.com>
2023-10-22 00:23:47 +0800

    feat: 建立開發環境

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+
+
+<template>
+  <header>
+    <div class="wrapper">
+      test
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+@media (min-width: 1024px) {
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
